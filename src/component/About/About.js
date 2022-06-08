6d61dcd6397abd18444ed879021734bd2a3392a9
KMNayem <imna41695@gmail.com>
2022-06-09 01:34:24 +0600

    adding some tools

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -3,9 +3,9 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>
+            <div>
                 about
-            </h1>
+            </div>
         </div>
     );
 };
--- src/component/Project/Projects.js
+++ src/component/Project/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Projects = () => {
     return (
         <div>
-            <h1>Project</h1>
+            Project 
         </div>
     );
 };
